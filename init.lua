9e55e73b868d0130c45c2da091ff986d8cf4294f
xcdnlgd <xcdnlgd@hotmail.com>
2025-04-02 16:51:16 +0800

    project local .nvimrc.lua, loaded  at startup

--- init.lua
+++ init.lua
@@ -2,3 +2,10 @@ require("option")
 require("keymap")
 require("autocmds")
 require("lazy_setup")
+
+-- create .nvimrc.lua in your project root folder
+local startup_project_config = vim.fn.getcwd() .. "/.nvimrc.lua"
+if vim.fn.filereadable(startup_project_config) == 1 then
+    dofile(startup_project_config)
+    vim.notify(".nvimrc.lua loaded")
+end
