a1ede0f785272ca8ad0e68cc8a1cb9f7f3a1cf7c
xcdnlgd <xcdnlgd@hotmail.com>
2025-04-03 22:13:04 +0800

    no autopair when followed by char

--- lua/plugins/blink_pairs.lua
+++ lua/plugins/blink_pairs.lua
@@ -1,3 +1,13 @@
+local not_followed_by_char = function(pair)
+  return function()
+    local cursor = vim.api.nvim_win_get_cursor(0)
+    local line = vim.api.nvim_get_current_line()
+    local col = cursor[2]
+    local char = line:sub(col + 1, col + 1)
+    return char == '' or char == ' ' or char == '\t' or char == pair
+  end
+end
+
 return {
   'xcdnlgd/blink.pairs',
   -- version = '*', -- (recommended) only required with prebuilt binaries
@@ -15,5 +25,25 @@ return {
         'Blue',
       },
     },
+    mappings = {
+      enabled = true,
+      pairs = {
+        ['('] = {
+          ')',
+          enter = false,
+          when = not_followed_by_char(')')
+        },
+        ['['] = {
+          ']',
+          enter = false,
+          when = not_followed_by_char(']')
+        },
+        ['{'] = {
+          '}',
+          enter = false,
+          when = not_followed_by_char('}')
+        },
+      }
+    }
   }
 }
--- lua/plugins/which_key.lua
+++ lua/plugins/which_key.lua
@@ -1,6 +1,5 @@
 return {
   "folke/which-key.nvim",
-  event = "VeryLazy",
   opts = function(_, opts)
     if not opts.icons then opts.icons = {} end
     opts.icons.group = ""
