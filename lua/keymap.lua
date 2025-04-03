270f66c8f2bbf2736d2f598d27c6b8c9ae1c5121
xcdnlgd <xcdnlgd@hotmail.com>
2025-04-03 20:18:59 +0800

    next/prev warning/error keymap
    from astronvim

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -6,6 +6,19 @@ vim.g.maplocalleader = "\\"
 
 local opts = { silent = true }
 
+local function diagnostic_jump(forward, severity)
+  local jump_opts = {}
+  if type(severity) == "string" then jump_opts.severity = vim.diagnostic.severity[severity] end
+  return function()
+    jump_opts.count = forward and vim.v.count1 or -vim.v.count1
+    vim.diagnostic.jump(jump_opts)
+  end
+end
+vim.keymap.set("n", "[e", diagnostic_jump(false, "ERROR"), { desc = "Previous error" })
+vim.keymap.set("n", "]e", diagnostic_jump(true, "ERROR"), { desc = "Next error" })
+vim.keymap.set("n", "[w", diagnostic_jump(false, "WARN"), { desc = "Previous warning" })
+vim.keymap.set("n", "]w", diagnostic_jump(true, "WARN"), { desc = "Next warning" })
+
 vim.keymap.set({ "n", "v" }, "<leader>l", "", { desc = "Language Tools" })
 vim.keymap.set("n", "<leader>t", "", { desc = "Terminal" })
 vim.keymap.set("n", "<leader>g", "", { desc = "Git" })
