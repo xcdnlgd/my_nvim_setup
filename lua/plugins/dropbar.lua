9da9e193f33480a7f0d6314921fc0869842524b4
xcdnlgd <xcdnlgd@hotmail.com>
2025-04-10 12:04:02 +0800

    dropbar disable folder

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -1,6 +1,13 @@
 return {
   'Bekaboo/dropbar.nvim',
   config = function()
+    require('dropbar').setup({
+      sources = {
+        path = {
+          max_depth = 1
+        }
+      }
+    })
     local dropbar_api = require('dropbar.api')
     vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
     vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
