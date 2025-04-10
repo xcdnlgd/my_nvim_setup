030c6854a6673f90ed049bd1ad49f664340258b7
xcdnlgd <xcdnlgd@hotmail.com>
2025-04-10 11:44:41 +0800

    lazyload neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,5 +1,11 @@
 return {
   "NeogitOrg/neogit",
+  cmd = {
+    "Neogit",
+    "NeogitCommit",
+    "NeogitLogCurrent",
+    "NeogitResetState",
+  },
   dependencies = {
     "nvim-lua/plenary.nvim",         -- required
     "sindrets/diffview.nvim",        -- optional - Diff integration
