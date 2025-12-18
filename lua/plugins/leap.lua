4ad84687116f011feb2d05442afc4692a76de9bd
xcdnlgd <xcdnlgd@hotmail.com>
2025-12-18 11:03:27 +0800

    update leap config

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -4,8 +4,8 @@ return {
     vim.api.nvim_set_hl(0, "LeapLabel", { link = "Search" })
   end,
   keys = {
-    { "s",  '<Plug>(leap-forward-to)',   mode = { 'n', 'x', 'o' } },
+    { "s",  '<Plug>(leap-forward)',   mode = { 'n', 'x', 'o' } },
     { 'gs', '<Plug>(leap-cross-window)', mode = { "n" } },
-    { 'S',  '<Plug>(leap-backward-to)',  mode = { 'n', 'x', 'o' } },
+    { 'S',  '<Plug>(leap-backward)',  mode = { 'n', 'x', 'o' } },
   }
 }
