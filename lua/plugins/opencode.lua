038bd74e73447ddbeeaa2f9956f9400f6688987d
xcdnlgd <xcdnlgd@hotmail.com>
2026-03-04 22:01:17 +0800

    opencode.nvim set to 0.4

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -1,11 +1,21 @@
 return {
   "nickjvandyke/opencode.nvim",
-  version = "*",
+  version = "0.4",
   config = function()
     vim.g.opencode_opts = {
-      -- Your configuration, if any; goto definition on the type or field for details
+      provider = {
+        tmux = {
+          options = "-h -l 80", -- Open in a horizontal split
+          focus = false, -- Keep focus in Neovim
+          -- Disables allow-passthrough in the tmux split
+          -- preventing OSC escape sequences from leaking into the nvim buffer
+          allow_passthrough = false,
+        },
+      }
     }
+    vim.o.autoread = true
 
-    vim.keymap.set({ "n", "x" }, "<leader>a", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
+    vim.keymap.set({ "n", "x" }, "<leader>a", function() require("opencode").ask("@this: ", { submit = true }) end,
+      { desc = "Ask opencode…" })
   end
 }
