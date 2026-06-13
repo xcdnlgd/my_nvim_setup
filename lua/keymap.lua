74bc78f26ef60620a01ccf0bbe97985e0c5eb68f
xcdnlgd <xcdnlgd@hotmail.com>
2026-06-13 22:46:39 +0800

    <S-Tab> remap=true

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -193,5 +193,5 @@ vim.keymap.set('i', '<C-a>', '<HOME>', { desc = 'Start of line' })
 vim.keymap.set('i', '<C-e>', '<END>', { desc = 'End of line' })
 
 -- st + tmux {extended-keys on}
-local all_mode = {"n", "i", "c", "t", "v", "x", "s", "o"}
-vim.keymap.set(all_mode, '<M-[>Z', '<S-Tab>', { })
+local all_mode = { "n", "i", "c", "t", "v", "x", "s", "o" }
+vim.keymap.set(all_mode, '<M-[>Z', '<S-Tab>', { remap = true })
